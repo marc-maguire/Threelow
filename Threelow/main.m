c56d848fcd05ada6709190111f4c0a2f0b92081e
Marc Maguire <maguire.marc@gmail.com>
2017-05-03 15:56:43 -0400

    initial commit

---
+++ Threelow/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Marc Maguire on 2017-05-03.
+//  Copyright © 2017 Marc Maguire. All rights reserved.
+//
+//
+//To play, a player rolls five dice.
+//The goal is to get the lowest score possible.
+//Each roll they must choose to hold at least one of the dice, but they can choose to hold as many as five dice.
+//If they have un-held dice, they re-roll until all five dice are held.
+//
+//Threes are worth 0 points (hence the name of the game),
+//every other roll is worth it's face value.
+//Whoever has the lowest score after everyone has had a complete turn wins the game (note: gambling is illegal).
+
+// ⚀ ⚁ ⚂ ⚃ ⚄ ⚅
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
